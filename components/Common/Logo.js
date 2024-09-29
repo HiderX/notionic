39df702c2c4efa9e715327ef5980dd5d19f0c9b8
hiderx <3193997905@qq.com>
2024-09-29 17:27:36 +0800

    logo

--- components/Common/Logo.js
+++ components/Common/Logo.js
@@ -2,7 +2,16 @@
 import * as React from 'react'
 
 const Logo = (props) => (
-  <img src='https://upyun.zer0.top/IMG_1360.jpeg'></img>
+  <img
+    width='24'
+    height='24'
+    viewBox='0 0 100 100'
+    style={{
+      opacity: 1
+    }}
+    src='https://upyun.zer0.top/IMG_1360.jpeg'
+  >
+  </img>
 )
 
 export default Logo
