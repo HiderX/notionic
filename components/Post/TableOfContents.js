ac4e551fad057d4db53786c2909fd88fd4fde9df
hiderx <3193997905@qq.com>
2024-10-08 21:24:39 +0800

    toc

--- components/Post/TableOfContents.js
+++ components/Post/TableOfContents.js
@@ -1,14 +1,10 @@
 import PropTypes from 'prop-types'
-import { useState, useEffect } from 'react'
 import { getPageTableOfContents } from 'notion-utils'
 import Link from 'next/link'
 import { ChevronLeftIcon } from '@heroicons/react/outline'
 import BLOG from '@/blog.config'
 
 export default function TableOfContents ({ blockMap, frontMatter, pageTitle }) {
-  const [expandedNodeIds, setExpandedNodeIds] = useState({})
-  const [nodes, setNodes] = useState([])
-
   let collectionId, page
   if (pageTitle) {
     collectionId = Object.keys(blockMap.block)[0]
@@ -17,54 +13,24 @@ export default function TableOfContents ({ blockMap, frontMatter, pageTitle }) {
     collectionId = Object.keys(blockMap.collection)[0]
     page = Object.values(blockMap.block).find(block => block.value.parent_id === collectionId).value
   }
-
-  useEffect(() => {
-    const nodes = getPageTableOfContents(page, blockMap)
-    setNodes(nodes)
-
-    // 初始化每个一级标题是否展开的状态
-    const initialExpandedState = {}
-    nodes.forEach(node => {
-      if (node.type === 'header') {
-        initialExpandedState[node.id] = false // 初始状态下折叠子标题
-      }
-    })
-    setExpandedNodeIds(initialExpandedState)
-  }, [page, blockMap])
-
-  // 滚动监听的 useEffect 外部，始终执行
-  useEffect(() => {
-    const handleScroll = () => {
-      const offset = 80
-      const currentScroll = window.scrollY
-
-      nodes.forEach(node => {
-        if (node.type === 'header') {
-          const headingElement = document.querySelector(`.notion-block-${node.id.replaceAll('-', '')}`)
-          if (headingElement) {
-            const rect = headingElement.getBoundingClientRect()
-            if (rect.top < offset && rect.bottom > offset) {
-              setExpandedNodeIds(prevState => ({
-                ...prevState,
-                [node.id]: true
-              }))
-            } else {
-              setExpandedNodeIds(prevState => ({
-                ...prevState,
-                [node.id]: false
-              }))
-            }
-          }
-        }
-      })
-    }
-
-    window.addEventListener('scroll', handleScroll)
-    return () => window.removeEventListener('scroll', handleScroll)
-  }, [nodes])
-
-  // 如果没有目录节点，返回空
+  const nodes = getPageTableOfContents(page, blockMap)
   if (!nodes.length) return null
+
+  /**
+   * @param {string} id - The ID of target heading block (could be in UUID format)
+   */
+  function scrollTo (id) {
+    id = id.replaceAll('-', '')
+    const target = document.querySelector(`.notion-block-${id}`)
+    if (!target) return
+    // `65` is the height of expanded nav
+    // TODO: Remove the magic number
+    const top = document.documentElement.scrollTop + target.getBoundingClientRect().top - 65
+    document.documentElement.scrollTo({
+      top,
+      behavior: 'smooth'
+    })
+  }
 
   return (
     <div
@@ -90,23 +56,6 @@ export default function TableOfContents ({ blockMap, frontMatter, pageTitle }) {
           >
             {node.text}
           </a>
-          {node.type === 'header' && expandedNodeIds[node.id] && (
-            <div className='ml-4'>
-              {nodes
-                .filter(subNode => subNode.parentId === node.id)
-                .map(subNode => (
-                  <div key={subNode.id} className='px-2 hover:bg-gray-200 hover:dark:bg-gray-700 rounded-lg'>
-                    <a
-                      data-target-id={subNode.id}
-                      className='block py-1 cursor-pointer'
-                      onClick={() => scrollTo(subNode.id)}
-                    >
-                      {subNode.text}
-                    </a>
-                  </div>
-                ))}
-            </div>
-          )}
         </div>
       ))}
     </div>
